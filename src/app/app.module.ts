449bef7015402d55e6c5816a396e63f5de6f32b4
esha8e <eshae8889@gmail.com>
2022-07-07 22:26:21 +0530

    component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DepartmentNameComponent } from './department-name/department-name.component';
+import { FacultynameComponent } from './facultyname/facultyname.component';
+import { StudentsComponent } from './students/students.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DepartmentNameComponent
+    DepartmentNameComponent,
+    FacultynameComponent,
+    StudentsComponent
   ],
   imports: [
     BrowserModule,
